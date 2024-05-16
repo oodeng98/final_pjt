38639973218917c1f2b84c5b281b50995487bb25
woojin <woojin>
2024-05-16 17:05:23 +0900

    community back add

--- back/finance_pjt/urls.py
+++ back/finance_pjt/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls')),
+    path('articles/',include('community.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
