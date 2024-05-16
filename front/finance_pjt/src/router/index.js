ffda3324511fbb8afe1a28e809d357d89bfd2c6e
LimKwangyoung <kokoz22@naver.com>
2024-05-16 17:54:53 +0900

    ADD: construct components

--- front/finance_pjt/src/router/index.js
+++ front/finance_pjt/src/router/index.js
@@ -1,13 +1,69 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from '@/views/HomeView'
+import ProductView from '@/views/ProductView'
+import ProductDetailView from '@/views/ProductDetailView'
+import ExchangeRateView from '@/views/ExchangeRateView'
+import MapView from '@/views/MapView'
+import CommunityView from '@/views/CommunityView'
+import CommunityDetailView from '@/views/CommunityDetailView'
+import LoginView from '@/views/LoginView'
+import SignUpView from '@/views/SignUpView'
+import ProfileView from '@/views/ProfileView'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: ProductView
+    },
+    {
+      path: '/product/:product_id',
+      name: 'productDetail',
+      component: ProductDetailView
+    },
+    {
+      path: '/exchange-rate',
+      name: 'exchangeRate',
+      component: ExchangeRateView
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: MapView
+    },
+    {
+      path: '/community',
+      name: 'community',
+      component: CommunityView
+    },
+    {
+      path: '/community/:article_id',
+      name: 'communityDetail',
+      component: CommunityDetailView
+    },
+    {
+      path: '/accounts/login',
+      name: 'logIn',
+      component: LoginView
+    },
+    {
+      path: '/accounts/signup',
+      name: 'signUp',
+      component: SignUpView
+    },
+    {
+      path: '/accounts/:user_id',
+      name: 'profile',
+      component: ProfileView
+    },
     // {
     //   path: '/about',
     //   name: 'about',
