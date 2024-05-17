c34eafffb2d083f0a29c484f0edc1a91009d058b
oodeng98 <oodeng98@naver.com>
2024-05-17 10:54:55 +0900

    error resolve(?)

--- front/finance_pjt/src/App.vue
+++ front/finance_pjt/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-  <RouterLink :to="{ name: 'community' }">Community</RouterLink> |
-  <RouterLink :to="{ name: 'logIn' }">logIn</RouterLink> |
-  <RouterLink :to="{ name: 'signUp' }">signUp</RouterLink> |
-  <RouterLink :to="{ name: 'exchangeRate' }">환율 계산기</RouterLink>
-  <RouterView />
+    <RouterLink :to="{ name: 'community' }">Community</RouterLink> |
+    <RouterLink :to="{ name: 'logIn' }">logIn</RouterLink> |
+    <RouterLink :to="{ name: 'signUp' }">signUp</RouterLink> |
+    <RouterLink :to="{ name: 'exchangeRate' }">환율 계산기</RouterLink>
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
