c59846869ef7195b5245e2bb64ebff8e0f3ea71b
oodeng98 <oodeng98@naver.com>
2024-05-17 11:45:38 +0900

    feat: ExchangeRate

--- front/finance_pjt/src/views/ExchangeRateView.vue
+++ front/finance_pjt/src/views/ExchangeRateView.vue
@@ -6,46 +6,67 @@
       작동하지 않습니다.
     </p>
     <select name="country" v-model="selectCountry" @change="change">
-      <option v-for="country in countries" :value="country"></option>
-      <option value="USA">미국 달러</option>
+      <option v-for="country in countries" :value="country">
+        {{ country.cur_nm }}
+      </option>
     </select>
-    <input type="text" v-model="foreignCurrency" />
+    <input type="text" v-model="foreignCurrency" @input="input1" />
     <br />
-    <input type="text" v-model="koreaCurrency" />
+    <input type="text" v-model="koreaCurrency" @input="input2" />
   </div>
 </template>
 
 <script setup>
 import axios from "axios";
 import { onMounted, ref } from "vue";
+import { useCommunityStore } from "@/stores/community";
+
+const countries = ref([]);
+const selectCountry = ref("");
+const foreignCurrency = ref("");
+const koreaCurrency = ref("");
+let changeRate = 1;
+let userTarget = "input1";
 
 onMounted(() => {
+  const store = useCommunityStore();
   axios({
     method: "get",
     url: "http://127.0.0.1:8000/finance/exchange_rate/",
-    // headers: {
-    //   Authorization: "",
-    // },
+    headers: {
+      Authorization: store.token,
+    },
   })
     .then((res) => {
-      const countries = ref([]);
-      for (data in res.data) {
-        countries.value.push(data.cur_nm);
+      for (const data of res.data) {
+        countries.value.push(data);
       }
-      console.log(res);
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-const selectCountry = ref("");
-const foreignCurrency = ref("");
-const koreaCurrency = ref("");
-
 const change = function () {
-  console.log("change");
-  console.log(country.value);
+  const target = countries.value.find((ele) => {
+    return ele.cur_nm === selectCountry.value.cur_nm;
+  });
+  changeRate = target.kftc_deal_bas_r;
+  if (userTarget == "input1") {
+    input1();
+  } else {
+    input2();
+  }
+};
+
+const input1 = function () {
+  koreaCurrency.value = (foreignCurrency.value * changeRate).toFixed(2);
+  userTarget = "input1";
+};
+
+const input2 = function () {
+  foreignCurrency.value = (koreaCurrency.value / changeRate).toFixed(2);
+  userTarget = "input2";
 };
 </script>
 
