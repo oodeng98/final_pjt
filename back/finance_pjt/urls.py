e5c0c54a6648d232833dfe36a13959f62265bbdd
oodeng98 <oodeng98@naver.com>
2024-05-17 14:25:31 +0900

    feat: profile page incomplete

--- back/finance_pjt/urls.py
+++ back/finance_pjt/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls')),
     path('articles/',include('community.urls')),
+    path('accounts/<int:user_id>/', views.profile),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- front/finance_pjt/src/App.vue
+++ front/finance_pjt/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-  <div>test</div>
+    <div>test</div>
     <RouterLink :to="{ name: 'community' }">Community</RouterLink> |
     <RouterLink :to="{ name: 'logIn' }">logIn</RouterLink> |
     <RouterLink :to="{ name: 'signUp' }">signUp</RouterLink> |
-    <RouterLink :to="{ name: 'exchangeRate' }">환율 계산기</RouterLink>
+    <RouterLink :to="{ name: 'exchangeRate' }">환율 계산기</RouterLink> |
+    <RouterLink :to="{ name: 'profile', params: { user_id: 1 } }"
+      >My Page</RouterLink
+    >
     <RouterView />
   </div>
 </template>
