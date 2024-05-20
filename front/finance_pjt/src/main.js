eab4d3b02dc80caef21dff0815e27bf96c49d9b2
woojin <woojin>
2024-05-20 13:29:05 +0900

    style: add bootstrap

--- front/finance_pjt/src/main.js
+++ front/finance_pjt/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import App from "./App.vue";
 import router from "./router";
 
@@ -13,5 +15,6 @@ pinia.use(piniaPluginPersistedstate);
 // app.use(createPinia())
 app.use(router);
 app.use(pinia);
+app.use(BootstrapVue3)
 
 app.mount("#app");
--- front/finance_pjt/src/views/HomeView.vue
+++ front/finance_pjt/src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h1>Home</h1>
+    <b-button variant="danger">Button</b-button>
+
     <RouterView />
   </div>
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
